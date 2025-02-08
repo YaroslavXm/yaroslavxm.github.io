b1fb10231e9f9e2740ea6c21b6cb05567f596243
Xmgam3s <xmstealernotify@gmail.com>
2025-02-08 11:35:44 +0500

    Global Update
    1.Deleted easter egg
    2.Changed title of site from "Xmgam3s Site" to "Xmgam3s's site"
    3.Added footer
    4.Added button for hide / show footer
    5.Added index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleVisibility() {
+    var block = document.getElementById('about');
+    if (block.style.display === 'none') {
+        block.style.display = 'block';
+    } else {
+        block.style.display = 'none';
+    }
+}
