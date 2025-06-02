7320f673f924e50dbafb71b624ce714602024080
Xmgam3s <xmstealernotify@gmail.com>
2025-06-02 22:09:24 +0500

    hh
    hh

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,3 @@
 function redirect(url) {
     location.replace(url)
 }
-
-/*
-function code() {
-    var codePanel = document.getElementById('code')
-    if (codePanel.value == 'RUJBVCcsVEkgTE9YIQ=='){
-        redirect("https://pastebin.com/fYQysmz9")
-    }
-    else{
-        alert("Not right")
-    }
-}
-*/
